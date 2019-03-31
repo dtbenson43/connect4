6baa6c7b84a621a862d73a56f663cabe3eb5f741
Daniel Benson <dbenson24@gmail.com>
2019-03-31 16:50:43 -0500

    Update Connect4.js

--- Connect4.js
+++ Connect4.js
@@ -8,37 +8,37 @@ class Connect4 {
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
       ],
-      turn: 1,
-      fuckingwow: 'unfuckingbelieveable'
+      turn: 1
     }
 
     this.matches = [];
 
     const temp = [];
-    temp.length = 7;
+    temp.length = 4;
     temp.fill('0');
     const empty = temp.join('');
 
-    for(let i = 0; i < Math.pow(2, 7); i++) {
+    for(let i = 1; i < Math.pow(2, 4); i++) {
       const bin = (i >>> 0).toString(2);
       const binary = (empty + bin).substring(bin.length);
       this.matches.push(binary);
     }
     this.matches.reverse();
 
-    console.log(this.matches);
-
     for(let i = 0; i < 7; i++) {
       const button = document.getElementById(`button${i}`);
       button.addEventListener('click', () => {
         if (this.state.turn === 1) {
           if(this.placeIntoColumn(i, 1)) {
-            if(!this.boardFull()) {
-              this.getColRowDiag(this.state.gameBoard);
-              this.setState({ turn: 2});
-              this.agentTurn();
+            if(!this.winCheck(this.state.gameBoard, 1)) {
+              if(!this.boardFull()) {
+                this.setState({ turn: 2});
+                this.agentTurn();
+              } else {
+                console.log('draw game');
+              }
             } else {
-              console.log('gameover');
+              console.log('you win');
             }
           }
         }
@@ -47,7 +47,7 @@ class Connect4 {
     this.render();
   }
 
-  getColRowDiag(board) {
+  getLines(board) {
     const height = board.length;
     const width = board[0].length;
 
@@ -60,21 +60,41 @@ class Connect4 {
       lines.push(temp);
     }
 
-    start = 6;
-    left = true;
-    for(let i = 0; i < 11; i++) {
-      const temp = [];
-      for(let j = start; j < width; j++) {
-        temp.push(gameBoard)
+    for(let i = 0; i < 2; i++) {
+      let row = i, col = 0, line = [];  
+      while(col < width && row < height) {
+        line.push(board[row++][col++]);
       }
+      lines.push(line);
+      row = i, col = width - 1, line = [];
+      while(col > -1 && row < height) {
+        line.push(board[row++][col--]);
+      }
+      lines.push(line);
     }
 
-    console.log(lines);
+    for(let i = 1; i < 4; i++) {
+      let row = 0, col = i, line = [];
+      while(col < width && row < height) {
+        line.push(board[row++][col++]);
+      }
+      lines.push(line);
+      row = 0, col = (width - 1) - i, line = [];
+      while(col > -1 && row < height) {
+        line.push(board[row++][col--]);
+      }
+      lines.push(line);
+    }
+
+    return lines;
   }
 
-  winCheck(num) {
-
-  }
+  winCheck(board, num) {
+    let temp = [];
+    temp.length = 4;
+    temp = temp.fill(`${num}`).join('');
+    return this.getLines(board).map(line => line.join('')).filter(line => line.includes(temp)).length;
+    }
 
   boardFull() {
     const { gameBoard } = this.state;
@@ -91,6 +111,45 @@ class Connect4 {
     return true;
   }
 
+  getScore(lines, num) {
+    let tokens = [];
+    lines.forEach(line => {
+      let temp = line.split(`${num}`);
+      temp.forEach(token => tokens.push(token))
+    });
+    tokens = tokens.filter(token => token.length > 3).map(token => {
+      const reg = num === 1 ? /2/g : /1/g;
+      let t = token.replace(reg, '1');
+      return t;
+    }).filter(token => token.includes('1'));
+    let score = 0;
+    tokens.forEach(token => {
+      this.matches.forEach(match => {
+        let idx = 1;
+        while(idx != -1) {
+          idx = token.indexOf(match);
+          if (idx != -1) {
+            let count = 0;
+            for(let i = idx; i < token.length; i++) {
+              count += parseInt(token.charAt(i));
+            }
+            token = token.replace(match, '0000');
+            score += count * count * count * count;
+          }
+        }
+      });
+    });
+    return score;
+  }
+
+  evaluate(board) {
+    const lines = this.getLines(board).map(line => line.join(''));
+    const playerScore = this.getScore(lines, 2);
+    const agentScore = this.getScore(lines, 1);
+    // console.log(playerScore, agentScore);
+    return agentScore - playerScore;
+  }
+
   agentTurn() {
     const { gameBoard } = this.state;
     const depthLimit = 5;
@@ -98,8 +157,8 @@ class Connect4 {
       const node = {
         board: [ ...board],
         cutoff,
-        applyAction: (action) => {
-          const newBoard = this.insertPiece(board, action, 2);
+        applyAction: (action, num) => {
+          const newBoard = this.insertPiece(board, action, num);
           return newNode(newBoard, cutoff - 1);
         }
       }
@@ -112,17 +171,13 @@ class Connect4 {
       return node;
     }
 
-    const evaluate = (board) => {
-      return Math.floor(Math.random() * 100);
-    }
-
     const minVal = (node, curAlpha, curBeta) => {
-      if (node.cutoff == 0) { // add check for win
-        return evaluate(node.board);
+      if (node.cutoff == 0 || this.winCheck(node.board, 1) || this.winCheck(node.board, 2)) { // add check for win
+        return this.evaluate(node.board);
       }
 
       node.actions.forEach((action) => {
-        curBeta = Math.min(curBeta, maxVal(node.applyAction(action), curAlpha, curBeta));
+        curBeta = Math.min(curBeta, maxVal(node.applyAction(action, 1), curAlpha, curBeta));
         if (curAlpha >= curBeta) {
           return curBeta;
         }
@@ -131,17 +186,17 @@ class Connect4 {
     }
 
     const maxVal = (node, curAlpha, curBeta) => {
-      if (node.cutoff == 0) { // add check for win
-        return evaluate(node.board);
+      if (node.cutoff == 0 || this.winCheck(node.board, 1) || this.winCheck(node.board, 2)) { // add check for win
+        return this.evaluate(node.board);
       }
       
       node.actions.forEach((action) => {
-        curBeta = Math.max(curAlpha, minVal(node.applyAction(action), curAlpha, curBeta));
+        curAlpha = Math.max(curAlpha, minVal(node.applyAction(action, 2), curAlpha, curBeta));
         if (curAlpha >= curBeta) {
-          return curBeta;
+          return curAlpha;
         }
       });
-      return curBeta;
+      return curAlpha;
     }
 
     const nextMove = () => {
@@ -151,8 +206,10 @@ class Connect4 {
       let nextAction = -1
 
       const firstNode = newNode(gameBoard, depthLimit);
+      // console.log(firstNode.actions);
       firstNode.actions.forEach((action) => {
-        alphaPrime = minVal(firstNode.applyAction(action), alpha, beta);
+        // console.log("column: " + action);
+        alphaPrime = minVal(firstNode.applyAction(action, 2), alpha, beta);
         if (alphaPrime > alpha) {
           alpha = alphaPrime;
           nextAction = action;
@@ -162,19 +219,25 @@ class Connect4 {
       return nextAction;
     }
 
-    console.log(nextMove());
-
-    const prom = new Promise((res, rej) => {
+    const move = nextMove();
+    if (move == -1) {
       let column = Math.floor(Math.random() * 7);
       while(!this.placeIntoColumn(column, 2)) {
         column = Math.floor(Math.random() * 7);
       }
-      res();
-    });
-    prom.then(() => {
-      this.setState({ turn: 1});
-    });
-    console.log('hello');
+    } else {
+      this.placeIntoColumn(move, 2);
+    }
+
+    if(!this.winCheck(this.state.gameBoard, 2)) {
+      if(!this.boardFull()) {
+        this.setState({ turn: 1});
+      } else {
+        console.log('draw game');
+      }
+    } else {
+      console.log('you lose');
+    }
   }
 
   insertPiece(gameBoard, col, num) {
@@ -215,26 +278,11 @@ class Connect4 {
         }
       });
     });
-    // let html = `<table style="width:100%; height:100%">`;
-    // gameBoard.forEach((row) => {
-    //   html = `${html}<thead>`
-    //   html = `${html}<tr>`;
-    //   row.forEach((col, idx) => {
-    //     html = `${html}
-    //     <td>
-    //       <div class="color${col}">${idx}</div>
-    //     </td>`
-    //   });
-    //   html = `${html}</tr>`;
-    // });
-    // html = `${html}</tbody></table>`;
-    // console.log(html);
-    // this.root.innerHTML = html;
   }
 
   static main() {
-    const game = new Connect4();
+    return new Connect4();
   }
 }
 
-Connect4.main();
+const game = Connect4.main();
